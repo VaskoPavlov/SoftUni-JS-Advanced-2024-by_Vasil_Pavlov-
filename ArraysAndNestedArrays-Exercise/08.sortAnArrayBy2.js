703dbfaaa1fccde7b613c2be8f5af98a21239c06
VaskoPavlov <vasko.pavlov9924@gmail.com>
2024-04-05 09:12:01 +0300

    Exercise 7 - sortAnArrayBy2.js

---
+++ ArraysAndNestedArrays-Exercise/08.sortAnArrayBy2.js
@@ -0,0 +1,26 @@
+/**
+ * Task: Write a function that orders a given array of strings, 
+ * by a length in ascending order as primary criteria, 
+ * and by alphabetical value in ascending order as second criteria. 
+ * The comparison should be case-insensitive.
+ * 
+ * The input comes as an array of strings.
+ * The output is the elements of the ordered array of strings, printed each on a new line.
+ */
+
+/**
+ * @param {string[]} array 
+ */
+function sort(array){
+    function compareAlphabetically(a,b){
+        return a.toLowerCase().localeCompare(b.toLowerCase());
+    }
+    array.sort(compareAlphabetically);
+    function compareString(a, b){
+        return a.length - b.length;
+    }
+    array.sort(compareString);
+    for(let i = 0; i < array.length; i++){
+        console.log(array[i]);
+    }
+}
