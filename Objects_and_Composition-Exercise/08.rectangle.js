6dbbce70c83c95a45ce1cc048dc9651a84a55bb0
VaskoPavlov <vasko.pavlov9924@gmail.com>
2024-04-25 11:06:42 +0300

    Exercise 8 - rectangle.js

---
+++ Objects_and_Composition-Exercise/08.rectangle.js
@@ -0,0 +1,18 @@
+/**
+ * Creates a rectangle object with specified dimensions and color.
+ * @param {number} width - Width of the rectangle.
+ * @param {number} height - Height of the rectangle.
+ * @param {string} color - Color of the rectangle.
+ * @returns {Object} - Rectangle object with properties width, height, color, and a method calcArea.
+ */
+
+function rectangle(width, height, color) {
+    return {
+        width,
+        height,
+        color: color.charAt(0).toUpperCase() + color.slice(1),
+        calcArea() {
+            return this.width * this.height;
+        }
+    };
+}
