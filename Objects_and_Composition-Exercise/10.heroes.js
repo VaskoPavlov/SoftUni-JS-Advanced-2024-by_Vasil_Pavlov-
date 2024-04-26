9bd0caeea9b1bffc1f9f6eeb6091d4fffdb8314c
VaskoPavlov <vasko.pavlov9924@gmail.com>
2024-04-26 09:44:59 +0300

    Exercise 10 - heroes.js

---
+++ Objects_and_Composition-Exercise/10.heroes.js
@@ -0,0 +1,43 @@
+/**
+ * Factory function to create mage and fighter hero objects.
+ * @returns {Object} - An object containing functions to create mage and fighter heroes.
+ */
+
+function solve() {
+
+    function mage(name) {
+        let hero = {
+            name,
+            health: 100,
+            mana: 100,
+            cast(spell) {
+                if (this.mana > 0) {
+                    this.mana--;
+                    console.log(`${this.name} cast ${spell}`);
+                } else {
+                    console.log(`${this.name} cannot cast ${spell}, insufficient mana!`);
+                }
+            }
+        };
+        return hero;
+    }
+
+    function fighter(name) {
+        let hero = {
+            name,
+            health: 100,
+            stamina: 100,
+            fight() {
+                if (this.stamina > 0) {
+                    this.stamina--;
+                    console.log(`${this.name} slashes at the foe!`);
+                } else {
+                    console.log(`${this.name} is too tired to fight!`);
+                }
+            }
+        };
+        return hero;
+    }
+
+    return { mage, fighter };
+}
