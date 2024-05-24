f00ddd158cf87dffc5914f1fabfeab3b1fa89254
VaskoPavlov <vasko.pavlov9924@gmail.com>
2024-05-24 09:08:09 +0300

    Previous exercise with updated task - solution.js

--- DOM_Manipulation_and_Events-Lab/07. Shopping-Cart/solution.js
+++ DOM_Manipulation_and_Events-Lab/07. Shopping-Cart/solution.js
@@ -1,3 +1,16 @@
+/**
+ * Task: You will be given some products that you should be able to add to your cart. 
+ * Each product will have a name, picture, and price.
+When the "Add" button is clicked, append the current product to the textarea in the following format: 
+"Added {name} for {money} to the cart.\n". The price must be fixed to the second digit.
+When the button "Checkout" is clicked, calculate the total money that you need to pay for the products 
+that are currently in your cart. Append the result to the textarea in the following format: 
+"You bought {list} for {totalPrice}."
+The list should contain only the unique products, separated by ", ". 
+The total price should be rounded to the second decimal point.
+Also, after clicking over "Checkout" and every from above is done you should disable all buttons. 
+(You can't add products or checkout again if once the checkout button is clicked).
+ */
 function solve() {
    const textareaRef = document.querySelector('textarea');
    const addBtnsRef = document.querySelectorAll('.add-product');
