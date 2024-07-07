70394eb8f2153026ba90ccfd17c4159df52acc25
VaskoPavlov <vasko.pavlov9924@gmail.com>
2024-07-07 21:06:23 +0300

    Exercise 2 - getPersons.js (added task req. for person)

--- Classes_and_Attributes-Lab/person.js
+++ Classes_and_Attributes-Lab/person.js
@@ -1,3 +1,18 @@
+/**
+ * Write a class that represents a personal record. It has the following properties, 
+ * all set from the constructor:
+•	firstName
+•	lastName
+•	age
+•	email
+And a method toString(), which prints a summary of the information. See the example for 
+formatting details.
+Input
+The constructor function will receive valid parameters.
+Output
+The toString()method should return a string in the following format:
+`{firstName} {lastName} (age: {age}, email: {email})`
+ */
 class Person {
     constructor(firstName, lastName, age, email) {
         this.firstName = firstName
