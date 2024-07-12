61dbb747cf68bac584e142e6f8ee45ef39318f27
VaskoPavlov <vasko.pavlov9924@gmail.com>
2024-07-12 23:11:37 +0300

    Exercise 5 - lenghtLimit.js

---
+++ Classes_and_Attributes-Exercise/lenghtLimit.js
@@ -0,0 +1,43 @@
+/**
+ * Task: Create a class Stringer, which holds the single string and a length property. 
+ * The class should be initialized with a string and an initial length. 
+ * The class should always keep the initial state of its given string.
+Name the two properties innerString and innerLength.
+There should also be functional for increasing and decreasing the initial length property.
+Implement function increase(length) and decrease(length), which manipulate the length property 
+with the given value.
+The length property is a numeric value and should not fall below 0. It should not throw any errors, 
+but if an attempt to decrease it below 0 is done, it should be automatically set to 0.
+You should also implement functionality for toString() function, which returns the string, 
+the object was initialized with. If the length of the string is greater than the length property, 
+the string should be cut from right to left, so that it has the same length as the length property, 
+and you should add 3 dots after it if such truncation was done.
+If the length property is 0, just return 3 dots.
+ */
+class Stringer {
+    constructor(innerString, innerLength) {
+        this.innerString = innerString;
+        this.innerLength = innerLength;
+    }
+
+    increase(length) {
+        this.innerLength += length;
+    }
+
+    decrease(length) {
+        this.innerLength -= length;
+        if (this.innerLength < 0) {
+            this.innerLength = 0;
+        }
+    }
+
+    toString() {
+        if (this.innerLength === 0) {
+            return '...';
+        } else if (this.innerLength < this.innerString.length) {
+            return this.innerString.slice(0, this.innerLength) + '...';
+        } else {
+            return this.innerString;
+        }
+    }
+}
